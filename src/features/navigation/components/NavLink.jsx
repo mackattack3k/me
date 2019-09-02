f9993eb5f922459f203d5a6a5ee0f398d7758500
mackattack3k <marcushanssonmh@gmail.com>
2019-09-02 23:24:43 +0200

    Theme in navigation links

--- src/features/navigation/components/NavLink.jsx
+++ src/features/navigation/components/NavLink.jsx
@@ -1,18 +1,23 @@
 import { Link } from '@reach/router';
 import React from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
 
-const NavLink = props => (
-  <Link
-    {...props}
-    getProps={({ isCurrent }) => {
-      return {
-        style: {
-          color: isCurrent ? 'black' : 'grey'
-        }
-      };
-    }}
-  />
-);
+const NavLink = props => {
+  const theme = useContext(ThemeContext);
+  return (
+    <Link
+      {...props}
+      getProps={({ isCurrent }) => {
+        return {
+          style: {
+            color: isCurrent ? theme.secondary.main : theme.primary.text
+          }
+        };
+      }}
+    />
+  );
+};
 
 NavLink.propTypes = {};
 
