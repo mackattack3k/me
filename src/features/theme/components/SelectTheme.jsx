fb94eb10030eca951b84db89c65ceee0e19f5f33
mackattack3k <marcushanssonmh@gmail.com>
2019-09-07 11:34:02 +0200

    Select theme checkbox with icon from fa

--- src/features/theme/components/SelectTheme.jsx
+++ src/features/theme/components/SelectTheme.jsx
@@ -1,23 +1,37 @@
+import { faCheckSquare, faSquare } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 import H1 from '../../text/components/H1';
 import { ThemeSelectorContext } from './ThemeSelectorProvider';
+
+const ToggleButton = styled.button`
+  padding: 5px 10px;
+  margin: 0;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  background: transparent;
+  color: ${props => props.theme.primary.text};
+`;
+
+const ToggleLabel = styled.span`
+  margin-left: 8px;
+`;
 
 const SelectTheme = () => {
   const { t } = useTranslation();
   const { isInDarkMode, setDarkMode } = useContext(ThemeSelectorContext);
+  const toggleDarkMode = () => setDarkMode(!isInDarkMode);
+  const checkedIcon = isInDarkMode ? faCheckSquare : faSquare;
   return (
     <>
       <H1>{t('theme.changeThemeTitle')}</H1>
-      <label htmlFor="dark-mode-checkbox">
-        <input
-          checked={isInDarkMode}
-          type="checkbox"
-          onChange={() => setDarkMode(!isInDarkMode)}
-          id="dark-mode-checkbox"
-        />
-        {t('theme.darkTheme')}
-      </label>
+      <ToggleButton type="button" onClick={toggleDarkMode}>
+        <FontAwesomeIcon icon={checkedIcon} />
+        <ToggleLabel>{t('theme.darkTheme')}</ToggleLabel>
+      </ToggleButton>
     </>
   );
 };
