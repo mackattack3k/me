e7fcac032564eb8e828a2d2b3f750db17bf85333
mackattack3k <marcushanssonmh@gmail.com>
2019-09-07 11:53:23 +0200

    Primary button design

---
+++ src/features/button/components/Button.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+  padding: 4px 12px;
+  font-size: 16px;
+  border: none;
+  margin: 0;
+  border-radius: 2px;
+  outline: none;
+  background: transparent;
+  color: ${props => props.theme.primary.text};
+  background: ${props => props.theme.primary.main};
+`;
+
+const Button = ({ onClick, children, type }) => (
+  <PrimaryButton onClick={onClick} type={type}>
+    {children}
+  </PrimaryButton>
+);
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node,
+  type: PropTypes.oneOf(['submit', 'button'])
+};
+Button.defaultProps = {
+  children: '',
+  type: 'button'
+};
+
+export default Button;
