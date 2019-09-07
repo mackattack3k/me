5f8c839a68edba2aefe459cdd7615e31ee3cb8f9
mackattack3k <marcushanssonmh@gmail.com>
2019-09-07 12:15:32 +0200

    Catching errors in language setup

--- src/features/language/initLanguage.js
+++ src/features/language/initLanguage.js
@@ -9,21 +9,29 @@ export const AVAILABLE_LANGUAGES = {
   SWEDISH: 'sv'
 };
 
-export const setupLanguage = () =>
-  i18n
-    .use(initReactI18next)
-    .use(LanguageDetector)
-    .init({
-      resources: {
-        [AVAILABLE_LANGUAGES.ENGLISH]: {
-          translation: english
+export const setupLanguage = () => {
+  try {
+    i18n
+      .use(initReactI18next)
+      .use(LanguageDetector)
+      .init({
+        resources: {
+          [AVAILABLE_LANGUAGES.ENGLISH]: {
+            translation: english
+          },
+          [AVAILABLE_LANGUAGES.SWEDISH]: {
+            translation: swedish
+          }
         },
-        [AVAILABLE_LANGUAGES.SWEDISH]: {
-          translation: swedish
+        fallbackLng: AVAILABLE_LANGUAGES.ENGLISH,
+        interpolation: {
+          escapeValue: false
         }
-      },
-      fallbackLng: AVAILABLE_LANGUAGES.ENGLISH,
-      interpolation: {
-        escapeValue: false
-      }
-    });
+      })
+      // eslint-disable-next-line no-console
+      .catch(e => console.log('Could not set up language', e));
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('Could not set up language', e);
+  }
+};
