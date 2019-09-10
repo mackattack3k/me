8f127bcbc5377ab9d2e2bb9604061425819ec156
mackattack3k <marcushanssonmh@gmail.com>
2019-09-10 19:01:33 +0200

    Lazy loading git section

--- src/features/home/components/HomePage.jsx
+++ src/features/home/components/HomePage.jsx
@@ -1,10 +1,11 @@
 import { Link } from '@reach/router';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import FakeAutoCode from '../../codepreview/components/FakeAutoCode';
+import LoadingSpinner from '../../loading/components/LoadingSpinner';
 import { STATISTICS_PAGE } from '../../statistics/statisticsRoutes';
-import GitSection from './GitSection';
 import HomeSection from './HomeSection';
+const GitSection = lazy(() => import('./GitSection'));
 
 const HomePage = () => {
   const { t } = useTranslation();
@@ -18,7 +19,9 @@ const HomePage = () => {
         subTitle={t('home.section2')}
         reverse
       >
-        <GitSection />
+        <Suspense fallback={<LoadingSpinner />}>
+          <GitSection />
+        </Suspense>
       </HomeSection>
       <HomeSection title={t('home.title3')} subTitle={t('home.section3')}>
         <Link to={STATISTICS_PAGE}>{t('home.click_here')}</Link>
