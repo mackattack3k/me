ec0af623c6585dda5aa59d53b2dcb08d662e2d31
mackattack3k <marcushanssonmh@gmail.com>
2019-09-10 17:07:52 +0200

    Using clickable link instead of redirect on home page

--- src/features/home/components/HomePage.jsx
+++ src/features/home/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Redirect } from '@reach/router';
+import { Link } from '@reach/router';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import FakeAutoCode from '../../codepreview/components/FakeAutoCode';
@@ -16,7 +16,7 @@ const HomePage = () => {
         git
       </HomeSection>
       <HomeSection title={t('home.title3')} subTitle={t('home.section3')}>
-        <Redirect to={STATISTICS_PAGE}>{t('home.click_here')}</Redirect>
+        <Link to={STATISTICS_PAGE}>{t('home.click_here')}</Link>
       </HomeSection>
     </>
   );
